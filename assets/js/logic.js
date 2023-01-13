2649ea35f4e88c424ae806aee23766e70c226a2a
splwebdev <shane_lynch@hotmail.com>
2023-01-13 23:53:36 +0000

    added check answer and remove button functions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -13,35 +13,54 @@ var initials = document.querySelector("#initials");
 var submitButton = document.querySelector("#submit");
 var feedback = document.querySelector("#feedback");
 
+// Element creation
+var olChoices = document.createElement("ol");
+choices.appendChild(olChoices);
+var lineBreak = document.createElement("hr");
+choices.appendChild(lineBreak);
+var answer = document.createElement("p")
+answer.setAttribute("id", "answer");
+answer.setAttribute("class", "hide");
+choices.appendChild(answer);
+
 // Other variables
 var timeLeft = 60;
 var index = 0;
+var score = 0;
 
 // Functions
 // Startgame function
 function startGame() { 
+  // sets startScreen element to hidden through adding a hide class
   startScreen.setAttribute("class", "hide");
+  // remove the hide class from the questions element
   questions.removeAttribute("class");
+  // calls the timer function
   startTimer();
+  // calls the showQuestion function
   showQuestion();
 }
 
 // Timer function
 function startTimer() { 
+  // sets the display for the timer class to equal the timeLeft variable (60 secs)
   timer.textContent = timeLeft;
+  // goes through and kicks off a countdown from 60 secs to 0 secs
   timeInterval = setInterval(function () {
     timeLeft--;
     timer.textContent = timeLeft;
+    // if the player runs out of time, it clears the time interval and then calls the end game function
     if(timeLeft === 0) {
       clearInterval(timeInterval);
       endGame();
     }
+    // TO DO: add condition if player answers all questions before time runs out
   }, 1000);
 };
 
 // Endgame function
 function endGame() {
-  
+  // TODO: add endgame function
 };
 
 // Question function
@@ -53,16 +72,44 @@ function showQuestion() {
     var choiceBtn = document.createElement("button");
     choiceBtn.setAttribute("id", "button" + i);
     choiceBtn.textContent = questionSource[index].choices[i];
-    choices.appendChild(choiceBtn);
+    // appends the buttons to the choices element based on the number of choices
+    olChoices.appendChild(choiceBtn);
   }
 }
 
 // Check answer function
 function checkAnswer(event) { 
   event.preventDefault();
+  var answer = document.querySelector("#answer");
+  answer.setAttribute("class", "");
 
-  if()
+  var chosenAnswer = event.target.textContent;
+  console.log(chosenAnswer);
+  var correctAnswer = questionSource[index].answer;
+  
+  for (i = 0; i < questionSource[index].answer.length; i++) {
+    if (correctAnswer === chosenAnswer) {
+      score++;
+      answer.textContent = "Correct!";
+    } else {
+      answer.textContent = "Wrong!";
+    }
+  }
+  index += 1;
+  if (index < questionSource.length) {
+    removeButton();
+    showQuestion();
+  } else {
+    endGame();
+  }
+}
 
+// remove button function
+function removeButton() {
+  for (i = 0; i < questionSource[index].choices.length; i++) {
+    var choiceBtn = document.getElementById("button" + i);
+    choiceBtn.parentNode.removeChild(choiceBtn);
+  }
 }
 
 // Obtain score function
@@ -83,30 +130,9 @@ function addScore() {
 // Submit score function
 function submitScore() { 
 
-}
-
-// add event listener / timer for questions
-// display questions
-
-
-// when question is answered display if correct or wrong
-// if question is wrong subtract time from clock
-
-
-// display new question set
-
-
-// if timer = 0 or all questions answered, game is over
-
-
-// display score and allow player to submit initials
-
-// Event Listeners
+}// Event Listeners
 // add event listener for on start
 startButton.addEventListener("click", startGame);
+// choiceBtn.addEventListener("click", checkAnswer);
+choices.addEventListener("click", checkAnswer);
 
-// add event listener for answer
-
-// add event listener for submitting score
-
-// 
